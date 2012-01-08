71d44de632ca79db927f553a71631b792a0fd707
marcioalthmann <marcio@marcioalthmann.net>
2012-01-08 21:51:00 -0200

    Codigo com dropdownlist em cascata com MVC

--- DeWebFormsParaMvc/MVC/Controllers/DropDownController.cs
+++ DeWebFormsParaMvc/MVC/Controllers/DropDownController.cs
@@ -18,10 +18,25 @@ namespace MVC.Controllers
             var autores = new List<Autor>
                               {
                                   new Autor {IdAutor = 1, Nome = "Fiódor Dostoiévski"},
-                                  new Autor {IdAutor = 1, Nome = "Friedrich Nietzsche"}
+                                  new Autor {IdAutor = 2, Nome = "Friedrich Nietzsche"}
                               };
 
             return View(new Livraria{Autores = autores});
+        }
+
+        public JsonResult ObterLivrosDoAutor(int id)
+        {
+            var livros = new List<Livro>
+                             {
+                                 new Livro{IdAutor = 2, IdLivro = 1, Titulo = "O Anticristo"},
+                                 new Livro{IdAutor = 2, IdLivro = 2, Titulo = "Assim Falou Zaratustra"},
+                                 new Livro{IdAutor = 2, IdLivro = 3, Titulo = "Para Além do Bem e do Mal"},
+                                 new Livro{IdAutor = 1, IdLivro = 4, Titulo = "Os Irmãos Karamazov"},
+                                 new Livro{IdAutor = 1, IdLivro = 5, Titulo = "O Jogador"},
+                                 new Livro{IdAutor = 1, IdLivro = 6, Titulo = "Recordações da Casa dos Mortos"}
+                             };
+
+            return Json(livros.Where(l => l.IdAutor == id), JsonRequestBehavior.AllowGet);
         }
 
     }
