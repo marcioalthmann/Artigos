a8e1b61b718186737fde16f51b1345445272f5bf
marcioalthmann <marcio@marcioalthmann.net>
2012-01-11 20:49:39 -0200

    Exemplo de utilizacao do plugin Tipsy

---
+++ JQuery/JQuery/Controllers/TipsyTooltipController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQuery.Controllers
+{
+    public class TipsyTooltipController : Controller
+    {
+        //
+        // GET: /TipsyTooltip/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
