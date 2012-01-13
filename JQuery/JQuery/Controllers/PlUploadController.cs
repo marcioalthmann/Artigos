c9340941923d0d39946bd19ca37ce1ab07040c2a
marcioalthmann <marcio@marcioalthmann.net>
2012-01-12 22:13:08 -0200

    Adicionado exemplo de utilizacao do plugin PlUpload

---
+++ JQuery/JQuery/Controllers/PlUploadController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQuery.Controllers
+{
+    public class PlUploadController : Controller
+    {
+        //
+        // GET: /PlUpload/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Upload(HttpPostedFileBase file)
+        {
+            // Lógica para salvar a foto
+        }
+
+    }
+}
